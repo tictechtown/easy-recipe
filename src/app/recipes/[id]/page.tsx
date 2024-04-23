99fb0d1fa78485f0decb0738051bdd201340a75a
TicTechTown <tictechtown@gmail.com>
2024-04-23 14:23:59 -0400

    Set edge runtime on production for Cloudflare

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -3,6 +3,9 @@ import { useRecipeListStore } from "@/app/lib/localStore";
 import Recipe from "@/app/ui/recipe";
 import { notFound, useRouter } from "next/navigation";
 
+export const runtime =
+  process.env.NODE_ENV === "production" ? "edge" : "nodejs";
+
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
   const { importedRecipes, removeRecipe } = useRecipeListStore(
