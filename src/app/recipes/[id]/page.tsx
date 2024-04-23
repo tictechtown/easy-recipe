3ea6260a00e923e79233f34036f80acfbf7fca90
TicTechTown <tictechtown@gmail.com>
2024-04-23 17:15:31 -0400

    Fix hydration

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -2,13 +2,21 @@
 import { useRecipeListStore } from "@/app/lib/localStore";
 import Recipe from "@/app/ui/recipe";
 import { notFound, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
+  const [isHydrated, setIsHydrated] = useState(false);
+
   const { importedRecipes, removeRecipe } = useRecipeListStore(
     (state) => state
   );
   const storedRecipe = importedRecipes.find((rcp) => rcp.id === params.id);
+
+  // Wait till Next.js rehydration completes
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
 
   const handleRemoveRecipe = () => {
     if (storedRecipe) {
@@ -20,5 +28,9 @@ export default function Page({ params }: { params: { id: string } }) {
   if (storedRecipe) {
     return <Recipe data={storedRecipe.recipe} onDelete={handleRemoveRecipe} />;
   }
-  return notFound();
+
+  if (params.id && isHydrated) {
+    return notFound();
+  }
+  return <main className="h-screen"></main>;
 }
