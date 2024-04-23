41f60528fc17a2aa092485de75f76e9f4fd1e757
TicTechTown <tictechtown@gmail.com>
2024-04-23 14:27:21 -0400

    Force edge runtime for now

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -3,8 +3,8 @@ import { useRecipeListStore } from "@/app/lib/localStore";
 import Recipe from "@/app/ui/recipe";
 import { notFound, useRouter } from "next/navigation";
 
-export const runtime =
-  process.env.NODE_ENV === "production" ? "edge" : "nodejs";
+export const runtime = "edge";
+// process.env.NODE_ENV === "production" ? "edge" : "nodejs";
 
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
