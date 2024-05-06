c30d7fd16796dc966e6255b18db2924382984490
TicTechTown <tictechtown@gmail.com>
2024-05-06 15:08:58 -0400

    Add PWA capabilities

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist({
+  // Your Next.js config
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,23 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  applicationName: "My Easy Recipes",
   title: "EasyRecipe",
   description: "Get your favorite web recipes all in one place",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "My Easy Recipes",
+    // startUpImage: [],
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
